e31a8e911fa25dd1392581dc6d35ed951090e591
ko_no <90126004+MrKono@users.noreply.github.com>
2026-04-09 20:31:17 +0900

    [add] convert recipe

--- src/main/java/com/github/gtexpert/gtmt/integration/storagedrawers/recipes/UpgradesLoader.java
+++ src/main/java/com/github/gtexpert/gtmt/integration/storagedrawers/recipes/UpgradesLoader.java
@@ -2,6 +2,8 @@ package com.github.gtexpert.gtmt.integration.storagedrawers.recipes;
 
 import static com.github.gtexpert.gtmt.integration.storagedrawers.items.StorageDrawersItems.upgradeStorageGT;
 
+import gregtech.api.unification.material.Materials;
+
 import net.minecraft.item.ItemStack;
 
 import gregtech.api.items.metaitem.MetaItem;
@@ -18,6 +20,7 @@ import com.github.gtexpert.gtmt.integration.storagedrawers.storageupgrades.Upgra
 public class UpgradesLoader {
 
     public static void upgradeStorage() {
+        // original
         if (StorageDrawersConfigHolder.removeOriginal) {
             ModHandler.removeRecipeByName(Mods.StorageDrawers.getResource("upgrade_storage_obsidian"));
             ModHandler.removeRecipeByName(Mods.StorageDrawers.getResource("upgrade_storage_iron"));
@@ -25,6 +28,7 @@ public class UpgradesLoader {
             ModHandler.removeRecipeByName(Mods.StorageDrawers.getResource("upgrade_storage_diamond"));
             ModHandler.removeRecipeByName(Mods.StorageDrawers.getResource("upgrade_storage_emerald"));
         }
+        // custom
         MetaItem.MetaValueItem[] fieldGenerators = new MetaItem.MetaValueItem[] {
                 MetaItems.FIELD_GENERATOR_LV, MetaItems.FIELD_GENERATOR_MV, MetaItems.FIELD_GENERATOR_HV,
                 MetaItems.FIELD_GENERATOR_EV, MetaItems.FIELD_GENERATOR_IV, MetaItems.FIELD_GENERATOR_LuV,
@@ -44,6 +48,27 @@ public class UpgradesLoader {
                     'S', new UnificationEntry(OrePrefix.screw, material),
                     'C', Mods.StorageDrawers.getItem("upgrade_template"),
                     'T', tier == -1 ? new UnificationEntry(OrePrefix.stickLong, material) : fieldGenerators[tier - 1]);
+            // convert recipe
+            if (material == Materials.Obsidian) {
+                addStorageUpgradeConvertRecipe(Mods.StorageDrawers.getItem("upgrade_storage", 1, 0), output, material);
+            }
+            if (material == Materials.Iron) {
+                addStorageUpgradeConvertRecipe(Mods.StorageDrawers.getItem("upgrade_storage", 1, 1), output, material);
+            }
+            if (material == Materials.Gold) {
+                addStorageUpgradeConvertRecipe(Mods.StorageDrawers.getItem("upgrade_storage", 1, 2), output, material);
+            }
+            if (material == Materials.Diamond) {
+                addStorageUpgradeConvertRecipe(Mods.StorageDrawers.getItem("upgrade_storage", 1, 3), output, material);
+            }
+            if (material == Materials.Emerald) {
+                addStorageUpgradeConvertRecipe(Mods.StorageDrawers.getItem("upgrade_storage", 1, 4), output, material);
+            }
         });
+    }
+
+    private static void addStorageUpgradeConvertRecipe(ItemStack original, ItemStack custom, Material material) {
+        ModHandler.addShapelessRecipe("original_to_custom/" + material.getUnlocalizedName(), original, custom);
+        ModHandler.addShapelessRecipe("custom_to_original/" + material.getUnlocalizedName(), custom, original);
     }
 }
