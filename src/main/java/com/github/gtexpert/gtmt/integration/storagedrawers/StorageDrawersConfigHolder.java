f06c8ac35b4f35d354ca270f1a0090b59e8c0f71
ko_no <90126004+MrKono@users.noreply.github.com>
2026-04-09 20:31:34 +0900

    [add] default material

--- src/main/java/com/github/gtexpert/gtmt/integration/storagedrawers/StorageDrawersConfigHolder.java
+++ src/main/java/com/github/gtexpert/gtmt/integration/storagedrawers/StorageDrawersConfigHolder.java
@@ -16,14 +16,29 @@ public class StorageDrawersConfigHolder {
 
     @Config.Comment({ "Specifies the materials that can be used to craft Storage Upgrades.",
             "Format: modId:materialName@multiplier$tier",
+            "==Notes==",
+            "--Material--",
+            "Material must have dust property",
+            "--multiplier--",
+            "The range of \"multiplier\" is 1 to 2147483647",
+            "The \"multiplier\" value is not affected by StorageDrawers's config.",
+            "--tier--",
             "\"tier\" represents the required field generator tier and can be set from 1 (LV) to 8 (UV).",
             "The \"tier\" field is optional and defaults to -1 (field generator is not required) if omitted.",
-            "Note: the \"multiplier\" value is not affected by StorageDrawers's config.",
-            "The range of \"multiplier\" is 1 to (2,147,483,647 / (1x1 Drawer Capacity * 64 * 7)) (default: 149796)",
+            "--others--",
             "Original materials (Obsidian/Iron/Gold/Diamond/Emerald) are automatically added with tier -1.",
             "Do not specify them here.",
             "The multiplier values for original materials are controlled by the StorageDrawers config.",
             "If this entry is left empty, only the original materials will be added." })
     public static String[] upgradeMaterials = new String[] {
+            "gregtech:wrought_iron@4", "gregtech:bronze@8",
+            "gregtech:steel@16", "gregtech:bismuth_bronze@32%1",
+            "gregtech:aluminium@64", "gregtech:rose_gold@128%2",
+            "gregtech:stainless_steel@256", "gregtech:ultimet@512%3",
+            "gregtech:titanium@1024", "gregtech:ruridit@2048%4",
+            "gregtech:tungsten_steel@4096", "gregtech:hssg@8192%5",
+            "gregtech:rhodium_plated_palladium@16384", "gregtech:europium@32768%6",
+            "gregtech:naquadah_alloy@65536", "gregtech:americium@131072%7",
+            "gregtech:darmstadtium@262144", "gregtech:neutronium@524288%8"
     };
 }
